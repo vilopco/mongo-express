08c8c00637e323f639537c73c913484d55dc218e
Christian <christianleyva@vilop.com>
2019-09-17 19:28:47 -0400

    Formulario

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
+app.use(express.urlencoded());
+
+
 var schema = mongoose.Schema({
-    name: {type: String, default: 'Anónimo'},
-    count: {type: Number, default: 1},
+    name: String,
+    email: String,
+    password: String
 });
 
 var visitante = mongoose.model('Visitor', schema);
@@ -13,12 +17,41 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1',
 
 mongoose.connection.on('error', (e)=>{console.error(e)});
 
+app.post('/', (req, res) => {
+    visitante.create({name: req.body.name,email:req.body.email,password:req.body.password},function(err){
+        if(err) return console.log(err);
+        visitante.find({},function(err, docs) {
+            let cadena='<a href="/register">Registrar</a><table><thead><tr><th>Name</th><th>Email</th></tr></thead><tbody>';
+            docs.forEach(element => {
+                cadena+="<tr><td>"+element.name+"</td><td>"+element.email+"</td></tr>"
+            });
+            cadena+="</tbody></table>";
+            res.send(cadena);
+        });
+    });
+});
+
 
 app.get('/', (req, res) => {
+    visitante.find({},function(err, docs) {
+        let cadena='<a href="/register">Registrar</a><table><thead><tr><th>Name</th><th>Email</th></tr></thead><tbody>';
+        docs.forEach(element => {
+            cadena+="<tr><td>"+element.name+"</td><td>"+element.email+"</td></tr>"
+        });
+        cadena+="</tbody></table>";
+        res.send(cadena);
+    });
+});
+
+app.get('/register', (req, res) => {
+    res.send('<form action="//localhost:3000/" method="post"><input type="text" name="name" placeholder="nombre"><input type="email" name="email" placeholder="Email"><input type="password" name="password" placeholder="Password"><input type="submit" value="Enviar"/></form>');
+    
+    
+    /*
     nombre = (req.query.name?req.query.name:"Anónimo");
     visitante.find({ name: nombre },function(err, docs) {
         if(docs.length>0){
-            visitante.updateOne({ _id: docs[0]._id }, { $set: { count: docs[0].count+1 }},function(){
+            visitante.update({ _id: docs[0]._id }, { $set: { count: docs[0].count+1 }},function(){
                 visitante.find({},function(err, docs) {
                     let cadena="<table><thead><tr><th>ID</th><th>Name</th><th>Visits</th></tr></thead><tbody>";
                     docs.forEach(element => {
@@ -33,26 +66,12 @@ app.get('/', (req, res) => {
             
             visitante.create({name: nombre},function(err){
                 if(err) return console.log(err);
-                visitante.find({},function(err, docs) {
-                    let cadena="<table><thead><tr><th>ID</th><th>Name</th><th>Visits</th></tr></thead><tbody>";
-                    console.log(docs);
-                    docs.forEach(element => {
-                        cadena+="<tr><td>"+element._id+"</td><td>"+element.name+"</td><td>"+element.count+"</td></tr>"
-                    });
-                    cadena+="</tbody></table>";
-                    res.send(cadena);
-                });
+                
             });
             
         }
     });
-
-    
-
-    
-
-    
-
+    */
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
