c907b0ff7feff30e50cf4a38ae3c381c8a8f2c4a
Christian <christianleyva@vilop.com>
2019-09-17 22:47:46 -0400

    Formulario

--- app.js
+++ app.js
@@ -20,14 +20,7 @@ mongoose.connection.on('error', (e)=>{console.error(e)});
 app.post('/', (req, res) => {
     visitante.create({name: req.body.name,email:req.body.email,password:req.body.password},function(err){
         if(err) return console.log(err);
-        visitante.find({},function(err, docs) {
-            let cadena='<a href="/register">Registrar</a><table><thead><tr><th>Name</th><th>Email</th></tr></thead><tbody>';
-            docs.forEach(element => {
-                cadena+="<tr><td>"+element.name+"</td><td>"+element.email+"</td></tr>"
-            });
-            cadena+="</tbody></table>";
-            res.send(cadena);
-        });
+        res.redirect("/");
     });
 });
 
@@ -45,33 +38,6 @@ app.get('/', (req, res) => {
 
 app.get('/register', (req, res) => {
     res.send('<form action="//localhost:3000/" method="post"><input type="text" id="name" name="name" placeholder="nombre"><input type="email" id="email" name="email" placeholder="Email"><input type="password" id="password" name="password" placeholder="Password"><input type="submit" value="Enviar"/></form>');
-    
-    
-    /*
-    nombre = (req.query.name?req.query.name:"Anónimo");
-    visitante.find({ name: nombre },function(err, docs) {
-        if(docs.length>0){
-            visitante.update({ _id: docs[0]._id }, { $set: { count: docs[0].count+1 }},function(){
-                visitante.find({},function(err, docs) {
-                    let cadena="<table><thead><tr><th>ID</th><th>Name</th><th>Visits</th></tr></thead><tbody>";
-                    docs.forEach(element => {
-                        cadena+="<tr><td>"+element._id+"</td><td>"+element.name+"</td><td>"+element.count+"</td></tr>"
-                    });
-                    cadena+="</tbody></table>";
-                    res.send(cadena);
-                });
-            });
-            return;
-        }else{
-            
-            visitante.create({name: nombre},function(err){
-                if(err) return console.log(err);
-                
-            });
-            
-        }
-    });
-    */
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
